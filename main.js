9a10d2e606f48f8517da45a2953da5d9e782ac2b
Craig Dennis <craigmdennis@gmail.com>
2019-01-29 22:22:19 +0000

    Abstract the code a little

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function fit(selection, command) {
                     // Reisze the width
                     case 'width': {
                         if (itemBounds.width !== parentBounds.width) {
-                            move(item, parent, command, artboard, selection);
+                            move(item, parent, command, artboard);
                             resize(item, parentBounds.width, itemBounds.height);
                         }
                         break; 
@@ -58,7 +58,7 @@ function fit(selection, command) {
                     case 'height': {
 
                         if (itemBounds.height !== parentBounds.height) {
-                            move(item, parent, command, artboard, selection);
+                            move(item, parent, command, artboard);
                             resize(item, itemBounds.width, parentBounds.height);
                         }
                         break; 
@@ -67,7 +67,7 @@ function fit(selection, command) {
                     // Resize both
                     default: {
                         if ((itemBounds.width !== parentBounds.width) || (itemBounds.height !== parentBounds.height)) {
-                            move(item, parent, command, artboard, selection);
+                            move(item, parent, command, artboard);
                             resize(item, parentBounds.width, parentBounds.height);
                         }
                         break; 
@@ -79,7 +79,7 @@ function fit(selection, command) {
 }
 
 // Calculate whether text is point or area
-function textCalc(item, offset, adjustment) {
+function adjustBasedOnText(item, offset, adjustment) {
     let calc;
 
     // If it's area
@@ -98,93 +98,113 @@ function textCalc(item, offset, adjustment) {
     return calc;
 }
 
-// Move based on the type of element
-function move(item, parent, command, artboard) {
-    const itemGlobalBounds = item.globalBounds;
-    const parentBounds = parent.boundsInParent;
-    const itemBounds = item.boundsInParent;
-    const itemWidth = itemBounds.width;
-    let artboardOffsetX = 0;
-    let artboardOffsetY = 0;
-    let offsetX = 0;
-    let offsetY = 0;
-    let calculatedOffsetX;
-    let calculatedOffsetY;
-    let x;
-    let y;
+// Calculate the correct offset for the Artboard
+// Or return 0 if it's not on an Artboard
+function calculateArtboardOffset(parent, artboard) {
+    let artboardOffset = {
+        x: 0,
+        y: 0
+    }
 
-    // If the selection is on an Artboard and not a Group
-    if (artboard) {
+    // If the selection is on an Artboard and is within a Group
+    if (artboard && parent && 'Group' === parent.constructor.name) {
 
-        if (parent && 'Group' === parent.constructor.name) {
-            const artboardBounds = artboard.boundsInParent;
-            artboardOffsetX = artboardBounds.x;
-            artboardOffsetY = artboardBounds.y;
+        const bounds = artboard.boundsInParent;
+        
+        artboardOffset = {
+            x: bounds.x,
+            y: bounds.y
         }
     }
 
-    calculatedOffsetX = -(itemGlobalBounds.x - artboardOffsetX - parentBounds.x);
-    calculatedOffsetY = -(itemGlobalBounds.y - artboardOffsetY - parentBounds.y);
+    return artboardOffset;
+}
+
+function calculateMovementOffset(item, parent, artboard, command) {
+    const artboardOffset = calculateArtboardOffset(parent, artboard);
+    const itemGlobalBounds = item.globalBounds;
+    const parentBounds = parent.boundsInParent;
+    const calc = {
+        x: itemGlobalBounds.x - artboardOffset.x - parentBounds.x,
+        y: itemGlobalBounds.y - artboardOffset.y - parentBounds.y
+    }
+    
+    // The offset always starts as 0
+    let offset = {
+        x: 0,
+        y: 0
+    }
     
     // Calculate offsets based on the direction of resize
     switch(command) {
         case 'width': {
-            offsetX = calculatedOffsetX;
-            offsetY = 0;
+            offset.x = -calc.x;
+            offset.y = 0;
             break;
         }
         case 'height': {
-            offsetX = 0;
-            offsetY = calculatedOffsetY;
+            offset.x = 0;
+            offset.y = -calc.y;
             break;
         }
         default: {
-            offsetX = calculatedOffsetX;
-            offsetY = calculatedOffsetY;
+            offset.x = -calc.x;
+            offset.y = -calc.y;
             break;
         }
     }
+
+    return offset;
+}
+
+// Determine how much an object needs to move
+function calculateMovement(item, offset) {
+    const itemWidth = item.boundsInParent.width;
+    let movement = {};
 
     // Calculate offsets based on the text alignment as the anchor point is different
     // https://adobexdplatform.com/plugin-docs/reference/scenegraph.html#texttextalign--string
     switch(item.textAlign) {
         case 'center': {
-            x = textCalc(item, offsetX, itemWidth/2);
-            y = offsetY;
+            movement.x = adjustBasedOnText(item, offset.x, itemWidth/2);
+            movement.y = offset.y;
             break;
         }
         case 'right': {
-            x = textCalc(item, offsetX, itemWidth);
-            y = offsetY;
+            movement.x = adjustBasedOnText(item, offset.x, itemWidth);
+            movement.y = offset.y;
             break;
         }
         default: {
-            x = offsetX;
-            y = offsetY;
+            movement.x = offset.x;
+            movement.y = offset.y;
             break;
         }
     }
 
+    return movement;
+}
+
+// Move the object
+function move(item, parent, command, artboard) {
+    const offset = calculateMovementOffset(item, parent, artboard, command);
+    const movement = calculateMovement(item, offset);
+    
     // Move the element by relative pixels
-    item.moveInParentCoordinates(x, y);
+    item.moveInParentCoordinates(movement.x, movement.y);
 }
 
 // Resize the object
 function resize(item, newWidth, newHeight) {
 
-    // If it's text
+    // Change point text to areaNox text and apply the dimensions
     if ('Text' === item.constructor.name) {
-
-        // Change from point text and resize
         item.areaBox = {
             width: newWidth,
             height: newHeight
         }
     
-    // If it's anything else
     } else {
-
-        // Resize as normal
         item.resize(newWidth, newHeight);
     }
 }
